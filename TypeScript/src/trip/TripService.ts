dd279fd401f29471f935cf4e5808f57593d26591
Kirsten Nordine <kirstennordine@gmail.com>
2021-12-01 19:45:07 -0500

    Replace isFriend variable with user.hasFriend()

--- TypeScript/src/trip/TripService.ts
+++ TypeScript/src/trip/TripService.ts
@@ -8,17 +8,9 @@ export default class TripService {
     public getTripsByUser(user: User): Trip[] {
         let tripList: Trip[] = [];
         const loggedUser: User = UserSession.getLoggedUser();
-        let isFriend: boolean = false;
 
         if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
+            if (user.hasFriend(loggedUser)) {
                 tripList = TripDAO.findTripsByUser(user);
             }
 
